410cf852afd84521349ce218ad082d4d8fee00e7
Ed Collins <Ed.Collins@bankofengland.co.uk>
2025-12-09 11:12:31 +0000

    Test.

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ provider "azurerm" {
   subscription_id = "0276e2c7-d8bc-4a73-b0df-3e9a289f4af4"
 }
 resource "azurerm_resource_group" "rg" {
-  name     = "ed-ci-cd-rg"
+  name     = "edc-ci-cd-rg"
   location = var.location
 }
